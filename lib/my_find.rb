1b5422fb3c81e93a1ff4c9851541154a99e70f46
Chrissy Hunt <chrissy.hunt@gmail.com>
2017-10-15 16:57:52 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -5,9 +5,7 @@ def my_find(collection)
 
     i = 0
     while i < collection.length
-      if yield(collection[i])
-        return yield(collection[i])
-      end
+      return yield(collection[i]) if yield(collection[i])
       i += 1
     end
 
