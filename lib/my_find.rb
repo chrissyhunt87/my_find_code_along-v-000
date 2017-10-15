228a4999ebf156089f0c068e8caec407f32a542f
Chrissy Hunt <chrissy.hunt@gmail.com>
2017-10-15 16:57:21 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,6 +3,14 @@ require 'pry'
 def my_find(collection)
   if block_given?
 
+    i = 0
+    while i < collection.length
+      if yield(collection[i])
+        return yield(collection[i])
+      end
+      i += 1
+    end
+
   else
     "No block given."
   end
